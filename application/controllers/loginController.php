e8412807c30995b112121b3cccf9cbf5aee52c5b
edisondavila <edisondavilacampos@gmail.com>
2020-12-05 18:10:46 -0500

    actualización

--- application/controllers/loginController.php
+++ application/controllers/loginController.php
@@ -6,11 +6,15 @@ class loginController extends CI_Controller {
     public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('clienteModel');
-		$this->load->model('loginModel');
-		$this->load->helper(array('form','url'));
+/* 		$this->load->model('clienteModel');
+        $this->load->model('loginModel');
+        
+        $this->load->helper(array('form','url'));
+        
 		$this->load->library('form_validation');
-		$this->output->set_header("Access-Control-Allow-Origin:*");
+        $this->output->set_header("Access-Control-Allow-Origin:*"); */
+        
+        
 		
 	}
     
